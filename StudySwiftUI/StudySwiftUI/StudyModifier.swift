fb5e0634ba7ba0ded70a9e06ee75599c9e2b261b
MacBook Pro <aaa@bb.com>
2024-03-17 02:25:32 +0800

    update: modifier

---
+++ StudySwiftUI/StudySwiftUI/StudyModifier.swift
@@ -0,0 +1,75 @@
+//
+//  StudyModifier.swift
+//  StudySwiftUI
+//
+//  Created by MacBook Pro on 3/17/24.
+//
+
+import SwiftUI
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
+    }
+}
+
+
+#Preview {
+    VStack {
+        VStack {
+            Text("Gryffindor")
+                .font(.largeTitle)
+            Text("Hufflepuff")
+            Text("Ravenclaw")
+            Text("Slytherin")
+        }
+        .font(.title) // 系统自定义的modifier
+        VStack {
+            Text("Gryffindor")
+                .blur(radius: 0)
+            Text("Hufflepuff")
+            Text("Ravenclaw")
+            Text("Slytherin")
+        }
+        .blur(radius: 5) // 系统自定义的modifier
+        Text("Hello World")
+            .modifier(Title()) // 直接调用modifier
+        Text("Hello World22") // 使用扩展间接调用modifier
+            .titleStyle()
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
+    }
+}
